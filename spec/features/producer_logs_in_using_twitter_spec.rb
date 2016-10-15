f5eec07357366d1d4dd2986b286d01e4c523583a
Ryan Workman <rdavid1099@gmail.com>
2016-10-15 11:52:39 -0600

    Set up omniauth-twitter

--- spec/features/producer_logs_in_using_twitter_spec.rb
+++ spec/features/producer_logs_in_using_twitter_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+
+RSpec.feature 'user can log in using Twitter' do
+  it 'clicks Login Using Twitter and is taken to their dashboard page' do
+    user = create_user
+    visit root_path
+    click_on 'Login Using Twitter'
+
+    expect(page).to have_content(user.username)
+    expect(page).to have_content('Logout')
+    expect(page).to have_content("Today's Top Trending Stories on CNN")
+    expect(page).to have_content("Your Recent Story Pitches")
+    expect(page).to have_current_path(viewer_dashboard_path)
+  end
+end
