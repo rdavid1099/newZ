f0f7c5ec4943471c6d005a3bd6c7bc19f2111a33
Ryan Workman <rdavid1099@gmail.com>
2016-10-16 11:13:13 -0600

    Adds model tests for users and stations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,16 @@
 class Station < ApplicationRecord
   has_many :stations_users
   has_many :users, through: :stations_users
+
+  validates :name, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true
+  validates :logo_path, presence: true
+
+  before_validation :set_logo
+
+  private
+
+  def set_logo
+    self.logo_path ||= 'default photo'
+  end
 end
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Station, type: :model do
+  it 'sets logo to default picture if image is not uploaded' do
+    station = Station.create(name: 'TestStation',
+                             url: 'http://www.teststation.com')
+
+    expect(station.logo_path).to eq('default photo')
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_presence_of(:url) }
+    it { is_expected.to validate_uniqueness_of(:url) }
+    it { is_expected.to have_many(:users) }
+  end
 end
