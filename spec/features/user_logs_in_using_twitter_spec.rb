c8d4e1d7be5ec7611707260e7af8ae99499ce9cc
Ryan Workman <rdavid1099@gmail.com>
2016-11-09 21:05:10 -0700

    Fix broken tests after styling

--- spec/features/user_logs_in_using_twitter_spec.rb
+++ spec/features/user_logs_in_using_twitter_spec.rb
@@ -15,7 +15,9 @@ RSpec.feature 'user can log in using Twitter' do
           role: 1
         ).first
         visit root_path
-        click_on 'Login Using Twitter'
+        within('.navbar') do
+          click_on 'Login Using Twitter'
+        end
 
         expect(user.producer?).to be(true)
         expect(page).to have_content('Tester Screen Name0')
@@ -36,7 +38,9 @@ RSpec.feature 'user can log in using Twitter' do
           location: 'Denver, CO'
         ).first
         visit root_path
-        click_on 'Login Using Twitter'
+        within('.navbar') do
+          click_on 'Login Using Twitter'
+        end
 
         expect(user.viewer?).to be(true)
         expect(page).to have_content('Tester Screen Name0')
@@ -58,7 +62,9 @@ RSpec.feature 'user can log in using Twitter' do
           role: 2
         ).first
         visit root_path
-        click_on 'Login Using Twitter'
+        within('.navbar') do
+          click_on 'Login Using Twitter'
+        end
 
         expect(user.admin?).to be(true)
         expect(page).to have_content('Tester Screen Name0')
